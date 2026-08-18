86d25d75a7f2bdd9986fe5d1a70290f46950d77d
BemvindoTech <bienvenu.mendy@univ-thies.sn>
2026-08-18 19:59:28 +0000

    Mise à jour du package.json et configuration finale

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // Ajoutez cette ligne pour le déploiement sur GitHub Pages
+  base: '/QuizzUp-Tech/', 
+  
   test: {
     environment: 'jsdom',
     globals: true,
